3dcae1bf2f5f5899ca5556369608fc28530d365f
RounitR <rounitrakesh365@gmail.com>
2025-06-01 04:56:30 +0530

    changes in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
 import CertificatePage from "./CertificatePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/certificate/:id" element={<CertificatePage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div
+            style={{ color: "#3070bf", textAlign: "center", marginTop: "40vh" }}
+          >
+            Welcome to Certificate Portal
+          </div>
+        }
+      />
+      <Route path="/certificate/:id" element={<Home />} />
+      <Route path="/certificate/:id/view" element={<CertificatePage />} />
+    </Routes>
   );
 }
 
