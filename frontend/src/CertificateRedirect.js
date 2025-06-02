efbc6072dafb1b20e43cc66b745a86678f4d91eb
RounitR <rounitrakesh365@gmail.com>
2025-06-02 19:11:48 +0530

    some files modified

--- frontend/src/CertificateRedirect.js
+++ frontend/src/CertificateRedirect.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const CertificateRedirect = () => {
@@ -6,16 +6,24 @@ const CertificateRedirect = () => {
   const queryParams = new URLSearchParams(location.search);
   const code = queryParams.get("q"); // gets 'NPTEL123'
 
- const handleViewCertificate = () => {
-   if (code) {
-     window.open(
-       `/NOC/NOC25/SEM1/Ecertificates/106/noc25-cs17/Course/${code}.pdf`,
-       "_blank"
-     );
-   } else {
-     alert("Invalid certificate code.");
-   }
- };
+  useEffect(() => {
+    const currentHost = window.location.hostname;
+    if (currentHost === "nptel.cc" && code) {
+      const newUrl = `https://internalapp.nptel.cc/noc/Ecertificate?q=${code}`;
+      window.location.href = newUrl;
+    }
+  }, [code]);
+
+  const handleViewCertificate = () => {
+    if (code) {
+      window.open(
+        `/NOC/NOC25/SEM1/Ecertificates/106/noc25-cs17/Course/${code}.pdf`,
+        "_blank"
+      );
+    } else {
+      alert("Invalid certificate code.");
+    }
+  };
 
   return (
     <>
